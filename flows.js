0d629d4760ab05acc1f6b112010fd2c8a8d86c60
bjarnemartens1708 <69972971+bjarnemartens1708@users.noreply.github.com>
2026-08-10 16:26:54 +0200

    Add files via upload

---
+++ flows.js
@@ -0,0 +1,7 @@
+// Which entry buttons appear on the flow-select screen.
+// Edit the values, commit, reload the page.
+window.FLOWS = {
+  "Fashion": true,
+  "Room Builder": true,
+  "Furniture Finder": true
+};
